d16173a4a9aa90c129b35b9b384a5e0e80b34fc8
Phisherman <grindchris@gmail.com>
2013-10-12 14:54:32 +0200

    Little tweak.
    Language translation and removing of unnecessary using-namespace
    directives.

--- ConvAdapt/ConvAdapt/FormMain.Designer.cs
+++ ConvAdapt/ConvAdapt/FormMain.Designer.cs
@@ -154,9 +154,9 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(6, 3);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.Size = new System.Drawing.Size(53, 13);
             this.label3.TabIndex = 7;
-            this.label3.Text = "Ersetzt";
+            this.label3.Text = "Replaced";
             // 
             // textBoxReplaced
             // 
